5f8994c179ddb95dea2cdce6801a18e4d718f35d
LuisFelipeAN <luis.felipe.almeida.nascimento@gmail.com>
2017-09-28 11:48:50 -0300

    comecando a aplicacao

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http")
+var url  = require("url")
+function inicia(rota, handlers){
+    function sobreRequisicao(requisicao, resposta){
+      rota(url.parse(requisicao.url).pathname,handlers,requisicao,resposta)
+    }
+    http.createServer(sobreRequisicao).listen(8888)
+    console.log("Servidor iniciado em http://localhost:8888");
+}
+exports.iniciaServidor = inicia
